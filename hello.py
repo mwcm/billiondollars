dacaf58043ed58da341f25808e1d2573216f63a8
mwcm <mwcmitchell@gmail.com>
2016-05-17 17:20:29 +0100

    fixed textarea

--- hello.py
+++ hello.py
@@ -1,10 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
+from wtforms import Form, TextField, TextAreaField
+
 app = Flask(__name__)
 app.debug = True
 
-@app.route('/')
+
+class BillionForm(Form):
+	body  = TextAreaField(u'Text')
+
+number = 100000
+numbers = [number] * 10000
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-	return render_template('index.html')
+	form = BillionForm(request.form)
+	return render_template("index.html",
+						form = form)
+
 
 if __name__ == '__main__' :
 	app.run()
