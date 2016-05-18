c4c98919f8c0f4665f4ac004da72a025b4ade4d8
mwcmitchell <mwcmitchell@gmail.com>
2016-05-17 21:07:31 -0400

    button almost works

--- hello.py
+++ hello.py
@@ -1,8 +1,11 @@
 from flask import Flask, request, render_template
 from wtforms import Form, TextField, TextAreaField
+from random import randint
 
 app = Flask(__name__)
 app.debug = True
+
+spent = []
 
 billiontxt = open("./static/billion.txt").read()
 
@@ -11,11 +14,27 @@ class BillionForm(Form):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-	form = BillionForm(request.form, "AEIOAEIOAEIOEAI")
-	return render_template("index.html",
-						form = form,
-						name = "aeiou")
+	if request.method == 'POST':
+		n = 0
+		for n in range(0,3):
+			r = randint(0,10000)
+			if r not in spent:
+				billiontxt.replace("100,000","       ", r)
+				spent.append(r)
+				n = n +1;
+				print(r)
 
+
+		form = BillionForm(request.form, "AEIOAEIOAEIOEAI")
+		return render_template("index.html",
+							form = form,
+							name = "aeiou")
+
+	if request.method == 'GET':
+		form = BillionForm(request.form, "AEIOAEIOAEIOEAI")
+		return render_template("index.html",
+							form = form,
+							name = "aeiou")
 
 if __name__ == '__main__' :
 	app.run()
