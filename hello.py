1bba74c7651b12482fa35c7dcf684d9c26f4843c
mwcm <mwcmitchell@gmail.com>
2016-05-18 20:16:47 +0100

    web sockets almost working?

--- hello.py
+++ hello.py
@@ -1,9 +1,14 @@
 from flask import Flask, request, render_template
+from flask.ext.socketio import SocketIO, emit
 from wtforms import Form, TextField, TextAreaField
 from random import randint
 
+#http://blog.miguelgrinberg.com/post/easy-websockets-with-flask-and-gevent
+#http://www.shanelynn.ie/asynchronous-updates-to-a-webpage-with-flask-and-socket-io/
+
 app = Flask(__name__)
 app.debug = True
+socketio = SocketIO(app)
 
 spent = []
 
@@ -12,29 +17,35 @@ billiontxt = open("./static/billion.txt").read()
 class BillionForm(Form):
 	body  = TextAreaField(default = billiontxt)
 
+
+@socketio.on('my event')
+def test_message(message):
+	emit('aeiou',{'data': 'got it!'})
+
 @app.route('/', methods=['GET', 'POST'])
-def index(billiontxt):
-	if request.method == 'POST':
-		n = 0
-		for n in range(0,3):
-			r = randint(0,10000)
-			if r not in spent:
-				billiontxt.replace("100,000","       ", r)
-				spent.append(r)
-				n = n +1;
-				print(r)
+def index():
+
+	# if request.method == 'POST':
+	# 	n = 0
+	# 	for n in range(0,3):
+	# 		r = randint(0,10000)
+	# 		if r not in spent:
+	# 			billiontxt.replace("100,000","       ", r)
+	# 			spent.append(r)
+	# 			n = n +1;
+	# 			print(r)
 
 
-		form = BillionForm(request.form, "AEIOAEIOAEIOEAI")
-		return render_template("index.html",
-							form = form,
-							name = "aeiou")
+	# 	form = BillionForm(request.form, "AEIOAEIOAEIOEAI")
+	# 	return render_template("index.html",
+	# 						form = form,
+	# 						name = "aeiou")
 
-	if request.method == 'GET':
+	if request.method == 'GET' or request.method == 'POST':
 		form = BillionForm(request.form, "AEIOAEIOAEIOEAI")
 		return render_template("index.html",
 							form = form,
 							name = "aeiou")
 
 if __name__ == '__main__' :
-	app.run()
+	socketio.run(app)
