7a85ff5309147127ba5e4c907f987b46b4649234
mwcm <mwcmitchell@gmail.com>
2016-05-19 20:41:38 +0100

    added comments with links used

--- hello.py
+++ hello.py
@@ -6,6 +6,8 @@ from gevent import monkey
 
 #http://blog.miguelgrinberg.com/post/easy-websockets-with-flask-and-gevent
 #http://www.shanelynn.ie/asynchronous-updates-to-a-webpage-with-flask-and-socket-io/
+#http://code.tutsplus.com/tutorials/creating-a-web-app-from-scratch-using-python-flask-and-mysql-part-6--cms-23402
+#http://code.tutsplus.com/tutorials/creating-a-web-app-from-scratch-using-python-flask-and-mysql--cms-22972
 
 
 monkey.patch_all()
@@ -26,31 +28,35 @@ class BillionForm(Form):
 @socketio.on('my event')
 def test_message(message):
 	emit('aeiou',{'data': 'got it!'})
-	print("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
+	#print("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
+	
 
 @app.route('/')
 def index():
-
-	# if request.method == 'POST':
-	# 	n = 0
-	# 	for n in range(0,3):
-	# 		r = randint(0,10000)
-	# 		if r not in spent:
-	# 			billiontxt.replace("100,000","       ", r)
-	# 			spent.append(r)
-	# 			n = n +1;
-	# 			print(r)
-
-
-	# 	form = BillionForm(request.form, "AEIOAEIOAEIOEAI")
-	# 	return render_template("index.html",
-	# 						form = form,
-	# 						name = "aeiou")
-
 		form = BillionForm(request.form, "AEIOAEIOAEIOEAI")
 		return render_template("index.html",
 							form = form,
 							name = "aeiou")
+
+
+
+@app.route('/lambo',methods=['POST'])
+def lambo():
+		form = BillionForm()
+		form.body = get_money()
+		return render_template('index.html', form = form)
+
+
+def get_money():
+	n = 0
+	for n in range(0,3):
+			r = randint(0,10000)
+			if r not in spent:
+				newtxt = billiontxt.replace("100,000","       ", r)
+				spent.append(r)
+				n = n +1;
+	return newtxt
+
 
 if __name__ == '__main__' :
 	socketio.run(app, debug = True)
