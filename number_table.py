c28554acfcfeac8cc03ce3834471430c97b0788e
mwcmitchell <mwcmitchell@gmail.com>
2016-09-29 00:49:14 -0400

    😎

--- number_table.py
+++ number_table.py
@@ -7,7 +7,6 @@ outfile = 'aeiou2.html'
 with open(infile, newline='\n', encoding='utf-8-sig') as f:
 
 	out = open(outfile, 'w')
-
 	content = str(f.readlines())
 	
 	content = content.replace('\\t','').replace('\\n','').replace('\\r','').replace('\'','').replace(',','').replace('100','100,').strip('[]')
