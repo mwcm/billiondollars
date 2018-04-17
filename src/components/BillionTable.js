512c780c9f6c6fb8ed8f42198924d4496f4c61ef
morgan <mwcmitchell@gmail.com>
2018-04-17 12:38:33 -0400

    changed to react app

---
+++ src/components/BillionTable.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class BillionTable extends Component {
+  render() {
+    return (
+      <div className="BillionTable">
+      
+      </div>
+    );
+  }
+}
+
+export default BillionTable;
