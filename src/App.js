efb05c6e3c54d56d7a248c176cc2519dcfd6438e
morgan <mwcmitchell@gmail.com>
2018-06-18 11:33:08 -0400

    moving to virtualized

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
-import Table from './components/Table.js'
+import {Column, Table} from 'react-virtualized'
+//import Table from './components/Table.js'
 import logo from './logo.svg';
+import 'react-virtualized/style.css' //only import once
 import './App.css';
 
+// <Table x={4} y={3999} />
 
 class App extends Component {
   render() {
@@ -16,7 +19,39 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
           <div style={{width:'100%'}}>
-            <Table x={4} y={3999} />
+
+            <Table
+              width={400}
+              height={300}
+              headerHeight={20}
+              rowHeight={30}
+              rowCount={list.length}
+              rowGetter={({ index }) => list[index]}
+            >
+              <Column
+                label='Col1'
+                dataKey='col1'
+                width={100}
+              />
+
+              <Column
+                label='Col2'
+                dataKey='col2'
+                width={100}
+              />
+
+              <Column
+                label='Col3'
+                dataKey='col3'
+                width={100}
+              />
+
+              <Column
+                label='Col4'
+                dataKey='col4'
+                width={100}
+              />
+            </Table>
           </div>
       </div>
     );
